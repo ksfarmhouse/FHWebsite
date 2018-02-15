225d140a3f5041200d97fd1859bd34a6e46312ef
Cameron Winter <cameronwinter27@gmail.com>
2018-02-14 19:49:57 -0600

    Started an .Net Core app to add new users
    Started an app to be run weekly to keep track of permanents

---
+++ AddUsers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using FHWebsite;
+
+namespace AddUsers
+{
+    /// <summary>
+    /// This is an app that will add users to the SQL database by reading names from a CSV file. 
+    /// Note that there is a wrapper class called SqlTools in the FHWebsite namespace designed to run SQL commands.
+    /// Also, use the connection string from FHWebsite to connect to the SQL database.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path;
+            string[] lines;
+            if (args.Length == 0)
+            {
+                Console.Write("Enter the path of a CSV file: ");
+                path = Console.ReadLine();
+            }
+            else
+            {
+                path = args[0];
+            }
+
+            Console.WriteLine("Reading file . . .");
+            try
+            {
+                lines = File.ReadAllLines(path);
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.ToString());
+                return;
+            }
+            
+            foreach(string line in lines)
+            {
+                
+            }
+
+        }
+    }
+}
---
+++ RoutineSignUp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FHWebsite;
+using System.Data;
+using System.Configuration;
+
+namespace RoutineSignUp
+{
+    class AddData
+    {
+        static void Main(string[] args)
+        {
+            string[] parameters = new string[] { "@Date" };
+            SqlDbType[] paramTypes = new SqlDbType[] { SqlDbType.Date };
+            string[] paramValues = new string[1];
+            DateTime firstDay = DateTime.Today;
+            string[] days = new string[] { "Mon", "Tue", "Wed", "Thu", "Fri" };
+            DateTime[] dates = new DateTime[5];
+
+            string command = @"INSERT INTO [dbo].[Meals]
+                                           ([UserID]
+                                           ,[Date]
+                                           ,[Dinner]
+                                           ,[Lunch]
+                                           ,[LQty]
+                                           ,[DQty])
+                                     SELECT [UserID], @Date, [{0}Dinner], [{0}Lunch], 1, 1
+	                                 FROM[dbo].[WeeklyMeals]
+                                        WHERE UserID = 7777";
+
+            // Configure everything
+            for(int i=0; i<days.Length; i++)
+            {
+                firstDay = firstDay.AddDays(1);
+                // Get the day to a weekday
+                while ((firstDay.DayOfWeek > DayOfWeek.Friday) | (firstDay.DayOfWeek < DayOfWeek.Monday))
+                {
+                    firstDay = firstDay.AddDays(1);
+                }
+                // Run the command
+                SqlTools.ExecuteNonQuery(ConfigurationManager.ConnectionStrings["FHConnectionString"].ConnectionString, String.Format(command, days[(int)firstDay.DayOfWeek - 1]), parameters, paramTypes, new string[] { firstDay.ToString("yyyy-MM-dd") });
+            }
+
+            
+
+        }
+    }
+}
