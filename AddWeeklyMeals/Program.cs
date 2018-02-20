3b8cf9cc04590cec71a946776091852fbe94a8e2
Cameron Winter <cjwinter@ksu.edu>
2018-02-20 14:45:48 -0600

    Added a add weekly meals application
    It's not really done yet

---
+++ AddWeeklyMeals/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FHWebsite;
+
+namespace AddWeeklyMeals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = "SELECT [ID] from [dbo].[Members]";
+            string connection;
+
+
+
+        }
+    }
+}
